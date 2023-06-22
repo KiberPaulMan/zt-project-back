cd713c17fb015e74d99312fdaa97c4365e0dfba0
Paul <twinencrypt@gmail.com>
2023-06-22 15:50:07 +0700

    Include admin app

--- employee/admin.py
+++ employee/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Employee
+from django.contrib.auth.models import User
+
+from .models import Employee, Department
 
 
 class EmployeeInline(admin.StackedInline):
@@ -9,3 +11,8 @@ class EmployeeInline(admin.StackedInline):
 
 class UserAdmin(BaseUserAdmin):
     inlines = [EmployeeInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Department)
--- ztproject/settings.py
+++ ztproject/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
