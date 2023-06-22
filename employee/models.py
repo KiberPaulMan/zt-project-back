b1b395c5f25ff61fffddbe9699ee68b8bc9c0a46
Paul <twinencrypt@gmail.com>
2023-06-22 15:07:19 +0700

    Add on_delete to employee model

--- employee/models.py
+++ employee/models.py
@@ -10,12 +10,12 @@ class Department(models.Model):
 
 
 class Employee(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     middle_name = models.CharField(max_length=20, blank=True, null=True, verbose_name='Отчество')
     date_of_birth = models.DateField(auto_now_add=True, verbose_name='Дата рождения')
     photo = models.ImageField(upload_to='employees/%Y/%m/%d/', blank=True, verbose_name='Фото')
     job_title = models.CharField(max_length=30, verbose_name='Должность')
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name='Подразделение')
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, verbose_name='Подразделение')
 
     def __str__(self):
         return self.user.username
