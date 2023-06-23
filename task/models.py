e8ff81fb16e41d78ba411bb0b0f226839e706639
Paul <twin>
2023-06-23 10:51:47 +0700

    Add fields to task model

--- task/models.py
+++ task/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from employee.models import Employee
 
 DATE_END = timezone.now() + timezone.timedelta(days=3)
 
@@ -16,7 +17,7 @@ class Task(models.Model):
         ('high', 'Высокий'),
     )
 
-    title = models.CharField(max_length=20, verbose_name='Наименование')
+    title = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     status = models.CharField(max_length=20,
                               choices=STATUS,
@@ -29,3 +30,22 @@ class Task(models.Model):
                                   default=timezone.now)
     date_finish = models.DateField(verbose_name='Дата окончания задачи',
                                    default=DATE_END)
+    supervisor = models.ForeignKey(Employee,
+                                   on_delete=models.SET_NULL,
+                                   related_name='supervisor_tasks',
+                                   verbose_name='Руководитель',
+                                   null=True)
+    responsible = models.ManyToManyField(Employee,
+                                         related_name='responsible_tasks',
+                                         verbose_name='Ответственный')
+    functional_group = models.ManyToManyField(Employee,
+                                              related_name='func_group_tasks',
+                                              verbose_name='Функциональная группа',
+                                              blank=True)
+    observers = models.ManyToManyField(Employee,
+                                       related_name='observers_tasks',
+                                       verbose_name='Наблюдатели',
+                                       blank=True)
+
+    def __str__(self):
+        return self.title[:50]
